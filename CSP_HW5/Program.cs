0d8be3fe6e2f11dd86605d795cb61ba104d17849
Bliznyuk Roman <romzes85@mail.ru>
2022-09-15 22:33:55 +0300

    ++

--- CSP_HW5/Program.cs
+++ CSP_HW5/Program.cs
@@ -2,60 +2,38 @@
 using System.Collections.Generic;
 using System.Deployment.Internal;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static CSP_HW5.Journal;
 
 namespace CSP_HW5
 {
-	//    Задание 1 только его
-	//Ранее в одном из практических заданий вы создавали
-	//класс «Журнал». Добавьте к уже созданному классу инфор-
-	//мацию о количестве сотрудников.Выполните перегрузку
-	//+ (для увеличения количества сотрудников на указанную
-	//величину), — (для уменьшения количества сотрудников
-	//на указанную величину), == (проверка на равенство ко-
-	//личества сотрудников), < и > (проверка на меньше или
-	//больше количества сотрудников), != и Equals.Используйте
-	//механизм свойств для полей класса.
-	public class Journal //где он ("Ранее в одном...") не нашёл
+    //    Задание 1 только его
+    //Ранее в одном из практических заданий вы создавали
+    //класс «Журнал». Добавьте к уже созданному классу инфор-
+    //мацию о количестве сотрудников.Выполните перегрузку
+    //+ (для увеличения количества сотрудников на указанную
+    //величину), — (для уменьшения количества сотрудников
+    //на указанную величину), == (проверка на равенство ко-
+    //личества сотрудников), < и > (проверка на меньше или
+    //больше количества сотрудников), != и Equals.Используйте
+    //механизм свойств для полей класса.
+    public class Journal //где он ("Ранее в одном...") не нашёл
     {
-        public int id = -1;
-        public class Employee
-		{
-			public int _id;
-			public string _name;
-			public Employee()
-			{
-				 _id = 0;
-				_name = "Smit";
-			}
-			public Employee(int id, string name)
-			{
-				_id = id;
-				_name = name;
-			}
-        }
-		public int _numberOfEmployees;
-		public Employee[] _employees;
+        public int _id = 0; //количество сотрудников 
 
-		public Journal()
-		{
-		    Employee[] employees = new Employee[1];
-			employees[0] = new Employee();
-            _numberOfEmployees = employees.Length;
+        public Journal()
+        {
 
         }
-        public Journal(int numberEmp, params string[] name)
-		{
-			Employee[] employees = new Employee[numberEmp];
-			
-			foreach (var item in employees)
-			{
-				item._id++;
-				item._name = name[id];
-			}
-            _numberOfEmployees = employees.Length;
+        public Journal(int id)
+        {
+            this._id = id;
         }
+        //public Journal()
+
         public override bool Equals(object obj)
         {
             return this.ToString() == obj.ToString();
@@ -66,59 +44,51 @@ namespace CSP_HW5
         }
         public static Journal operator +(Journal journal, int numberAdd)
         {
-			int oldSize = journal._employees.Length;
-            Array.Resize(ref journal._employees, oldSize + numberAdd);
-			for (int i = 0; i < numberAdd; i++)
-			{
-				oldSize++;
-                journal._employees[oldSize-1] = new Employee();
-            }
-            journal._numberOfEmployees = journal._employees.Length;
+            journal._id += numberAdd;
             return journal;
         }
         public static Journal operator -(Journal journal, int numberSub)
         {
-            int oldSize = journal._employees.Length;
-            Array.Resize(ref journal._employees, oldSize - numberSub);
-            journal._numberOfEmployees = journal._employees.Length;
+            journal._id -= numberSub;
             return journal;
         }
         public static bool operator ==(Journal j1, Journal j2)
         {
-            return j1._numberOfEmployees == j2._numberOfEmployees;
-
+            return j1._id == j2._id;
         }
         public static bool operator !=(Journal j1, Journal j2)
         {
-            return j1._numberOfEmployees != j2._numberOfEmployees;
+            return j1._id != j2._id;
         }
+
         public static bool operator >(Journal j1, Journal j2)
         {
-            return j1._numberOfEmployees > j2._numberOfEmployees;
+            return j1._id > j2._id;
         }
         public static bool operator <(Journal j1, Journal j2)
         {
-            return j1._numberOfEmployees < j2._numberOfEmployees;
+            return j1._id < j2._id;
         }
         public override string ToString()
         {
-            return  $"\nИмя сотрудника: { Journal.Employee._id } Его id: {Journal.Employee._name}\nЗаработная плата: {_salary}";
+            return $"колличество сотрудников: {this._id}";
         }
     }
 
 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
 
+            Journal j1 = new Journal(2);
+            Journal j2 = new Journal(3);
 
+            Console.WriteLine($"журнал J1 -{j1.ToString()}\tжурнал J2 -{j2.ToString()}\n");
+            j1 += 2;
+            Console.WriteLine("j1 += 2\n");
+            Console.WriteLine($"журнал J1 -{j1.ToString()}\tжурнал J2 -{j2.ToString()}\n");
 
-
-
-
-
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-
-		}
-	}
+        }
+    }
 }
