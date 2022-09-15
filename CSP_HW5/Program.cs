3801a5e3308a03068e0340030ca1a0c646a36d20
Bliznyuk Roman <romzes85@mail.ru>
2022-09-15 21:18:32 +0300

    ++

--- CSP_HW5/Program.cs
+++ CSP_HW5/Program.cs
@@ -19,13 +19,14 @@ namespace CSP_HW5
 	//механизм свойств для полей класса.
 	public class Journal //где он ("Ранее в одном...") не нашёл
     {
-		public class Employee
+        public int id = -1;
+        public class Employee
 		{
 			public int _id;
 			public string _name;
 			public Employee()
 			{
-				_id = 0;
+				 _id = 0;
 				_name = "Smit";
 			}
 			public Employee(int id, string name)
@@ -37,17 +38,17 @@ namespace CSP_HW5
 		public int _numberOfEmployees;
 		public Employee[] _employees;
 
-		Journal()
+		public Journal()
 		{
-			Employee[] employees = new Employee[1];
+		    Employee[] employees = new Employee[1];
 			employees[0] = new Employee();
             _numberOfEmployees = employees.Length;
 
         }
-		Journal(int numberEmp, params string[] name)
+        public Journal(int numberEmp, params string[] name)
 		{
 			Employee[] employees = new Employee[numberEmp];
-			int id = -1;
+			
 			foreach (var item in employees)
 			{
 				item._id++;
@@ -66,12 +67,43 @@ namespace CSP_HW5
         public static Journal operator +(Journal journal, int numberAdd)
         {
 			int oldSize = journal._employees.Length;
-            Array.Resize(ref journal._employees, numberAdd);
-			journal._employees[oldSize] = new Employee();
+            Array.Resize(ref journal._employees, oldSize + numberAdd);
+			for (int i = 0; i < numberAdd; i++)
+			{
+				oldSize++;
+                journal._employees[oldSize-1] = new Employee();
+            }
+            journal._numberOfEmployees = journal._employees.Length;
             return journal;
         }
+        public static Journal operator -(Journal journal, int numberSub)
+        {
+            int oldSize = journal._employees.Length;
+            Array.Resize(ref journal._employees, oldSize - numberSub);
+            journal._numberOfEmployees = journal._employees.Length;
+            return journal;
+        }
+        public static bool operator ==(Journal j1, Journal j2)
+        {
+            return j1._numberOfEmployees == j2._numberOfEmployees;
 
-
+        }
+        public static bool operator !=(Journal j1, Journal j2)
+        {
+            return j1._numberOfEmployees != j2._numberOfEmployees;
+        }
+        public static bool operator >(Journal j1, Journal j2)
+        {
+            return j1._numberOfEmployees > j2._numberOfEmployees;
+        }
+        public static bool operator <(Journal j1, Journal j2)
+        {
+            return j1._numberOfEmployees < j2._numberOfEmployees;
+        }
+        public override string ToString()
+        {
+            return  $"\nИмя сотрудника: { Journal.Employee._id } Его id: {Journal.Employee._name}\nЗаработная плата: {_salary}";
+        }
     }
 
 
